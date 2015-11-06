ae870e93b88466878815d860ae866cf1f12586f0
Thomas Lee <lisihan969@gmail.com>
2015-11-06 23:11:09 +0800

    Correct qam_send

--- 4/qam_send.m
+++ 4/qam_send.m
@@ -2,21 +2,27 @@
 % Average power = 1.
 function signals = qam_send(symbols, f_carrier, oversample_rate)
     len = length(symbols);
-    pad = mod(len, 4);
-    if pad
+    left = mod(len, 4);
+    if left
         if symbols(1) == 0
             warning 'Symbols(1) == 0, but a zero will be added in front of it.';
         end
-        symbols = [zeros(pad, 1); symbols];
+        symbols = [zeros(4 - left, 1); symbols];
+        len = length(symbols);
     end
 
     % Serial => parallel.
+    symbols = reshape(symbols, 2, len / 2)';
+
+    % 2 -> L
     signals = qam_2l_convert(symbols, 2 * f_carrier * oversample_rate);
     signal_len = size(signals, 1);
 
-    carrier = [cos(pi / oversample_rate * 1:signal_len)
-               cos(pi / oversample_rate * 1:signal_len + pi / 2)]';
+    % Get on carrier.
+    carrier = [cos(pi / oversample_rate * (1:signal_len))
+               cos(pi / oversample_rate * (1:signal_len) + pi / 2)]';
     signals = signals .* carrier;
 
+    % Merge.
     signals = signals(:, 1) .* signals(:, 2);
 end
