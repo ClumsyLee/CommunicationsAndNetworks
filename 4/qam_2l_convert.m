de038d3995bdc2b2d6350415a4c675355d33a2a2
Thomas Lee <lisihan969@gmail.com>
2015-11-07 02:00:34 +0800

    Simplify the implementation of qam_2l_convert

--- 4/qam_2l_convert.m
+++ 4/qam_2l_convert.m
@@ -1,14 +1,13 @@
-function signals = qam_2l_convert(symbols, sample_rate)
+function signals = qam_2l_convert(symbols)
     [len, cols] = size(symbols);
     if mod(len, 2)
         error 'qam_2l_convert: mod(size(symbols, 1), 2) ~= 0'
     end
 
-    signal_len = len / 2 * cols;
     symbols = symbols(:);
-    signals = zeros(signal_len, 1);
+    signals = zeros(len / 2, cols);
 
-    for k = 1:signal_len
+    for k = 1:len
         signals(k) = 2 * symbols(2 * k - 1) + symbols(2 * k);
     end
 
@@ -17,8 +16,5 @@ function signals = qam_2l_convert(symbols, sample_rate)
     signals(signals == 3) = 1;
     signals(signals == 2) = 3;
 
-    signals = sqrt(10) / 40 * signals;  % Make sure averge power = 1 in QAM.
-    signals = repmat(signals', [sample_rate, 1]);  % Repeat sample_rate times.
-
-    signals = reshape(signals, len / 2 * sample_rate, cols);
+    signals = signals / sqrt(10);  % Make sure averge power = 1 in QAM.
 end
