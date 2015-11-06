8fe1f42713a15f8f39801da4097de8a9c2e486a0
Thomas Lee <lisihan969@gmail.com>
2015-11-07 02:04:02 +0800

    Implement qam_l_judge

---
+++ 4/qam_l_judge.m
@@ -0,0 +1,19 @@
+function symbols = qam_l_judge(signals)
+    signals = sqrt(10) * signals;
+    signals(        signals < -2)   = 0;
+    signals(-2   <= signals < -0.5) = 1;
+    signals(-0.5 <= signals < 0.5)  = -1;
+    signals(0.5  <= signals < 2)    = 3;
+    signals(2    <= signals)        = 2;
+
+    [signal_len, cols] = size(signals);
+    signals = signals(:);
+
+    len = 2 * signal_len;
+    symbols = zeros(len, cols);
+
+    for k = 1:len
+        symbols(2 * k - 1) = floor(signals(k) / 2);
+        symbols(2 * k) = mod(signals(k), 2);
+    end
+end
