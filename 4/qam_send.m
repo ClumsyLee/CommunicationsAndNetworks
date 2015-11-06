4e4110f8720253d27edee038420a407b59cbec1f
Thomas Lee <lisihan969@gmail.com>
2015-11-07 02:05:13 +0800

    Draw in qam_send

--- 4/qam_send.m
+++ 4/qam_send.m
@@ -2,6 +2,8 @@
 % Average power = 1.
 function signals = qam_send(symbols, f_carrier, oversample_rate)
     len = length(symbols);
+    sample_rate = 8 * f_carrier * oversample_rate;
+
     left = mod(len, 4);
     if left
         if symbols(1) == 0
@@ -15,17 +17,25 @@ function signals = qam_send(symbols, f_carrier, oversample_rate)
     symbols = reshape(symbols, 2, len / 2)';
 
     % 2 -> L
-    signals = qam_2l_convert(symbols, 4 * f_carrier * oversample_rate);
-    signal_len = size(signals, 1);
-    % figure
-    % plot(signals);
+    signals = qam_2l_convert(symbols);
+    figure
+    plot(signals);
+    title 'After 2->L'
 
-    signals = filter(ones(1, 2 * oversample_rate), 1, signals);  % Low pass.
+    lpf = rcosdesign(0.5, 6, sample_rate);
+    signals = upfirdn(signals, lpf, sample_rate);
+    signal_len = size(signals, 1);
+    figure
+    plot(signals);
+    title 'After LPF'
 
     % Get on carrier.
     carrier = [cos(pi / oversample_rate * (1:signal_len))
                cos(pi / oversample_rate * (1:signal_len) + pi / 2)]';
     signals = signals .* carrier;
+    figure
+    plot(signals);
+    title 'Got on carrier'
 
     % Merge.
     signals = signals(:, 1) + signals(:, 2);
