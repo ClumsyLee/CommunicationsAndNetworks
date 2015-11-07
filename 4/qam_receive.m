00410068fd00fdb7533c9b4b34edf5349bb1ccd8
Thomas Lee <lisihan969@gmail.com>
2015-11-07 13:12:53 +0800

    carrier should be 2x for real signals when receiving

--- 4/qam_receive.m
+++ 4/qam_receive.m
@@ -6,8 +6,8 @@ function symbols = qam_receive(signals, f_carrier, oversample_rate, method)
     % Recover.
     switch method
     case 'real'
-        carrier = [cos(pi / oversample_rate * (1:signal_len))
-                   cos(pi / oversample_rate * (1:signal_len) + pi / 2)]';
+        carrier = 2 * [cos(pi / oversample_rate * (1:signal_len))
+                       cos(pi / oversample_rate * (1:signal_len) + pi / 2)]';
     case 'complex'
         carrier = [exp(-j * pi / oversample_rate * (1:signal_len))
                    exp(-j * (pi / oversample_rate * (1:signal_len) + pi / 2))]';
@@ -16,13 +16,11 @@ function symbols = qam_receive(signals, f_carrier, oversample_rate, method)
 
     % LPF
     lpf = rcosdesign(0.5, 6, sample_rate);
-    eyediagram(filter(lpf, 1, signals), sample_rate, 1, 3);
+    % eyediagram(filter(lpf, 1, signals), sample_rate, 1, 3);
     signals = upfirdn(signals, lpf, 1, sample_rate);
     signals = signals(7:end-6, :);  % Caused by filter delay.
 
     % Judge & merge.
     symbols = qam_l_judge(signals)';
     symbols = symbols(:);
-
-    symbols = symbols(find(symbols, 1):end);  % Remove paddings.
 end
